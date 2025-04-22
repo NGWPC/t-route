39e68170b5217f0e359f27a262ce08637c4ea1e5
Tadd Bindas <Tadd.N.Bindas@rtx.com>
2025-04-22 09:15:32 -0400

    JIRA 5916: Port forecast formatting code into T-Route RnR workspace package (#11)
    * initial commit
    
    * patch: updated functions to make code more testable. Added error handling
    
    * pr changes
    
    * changing name of variable in main to be consistent
    
    * feat: pydantic updates
    
    * fix: commited pydantic changes too soon
    
    ---------
    
    Co-authored-by: Quercus <quercus.hamlin@outlook.com>

---
+++ src/troute-rnr/src/troute_rnr/__init__.py
@@ -0,0 +1,4 @@
+from troute_rnr.format import format_xml, get_site_data, pull_nwm_inputs
+from troute_rnr.utils import get
+
+__all__ = ["get", "run", "format_xml", "pull_nwm_inputs", "get_site_data"]
